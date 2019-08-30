ff9119338665f718d93e145e3bd0f1082feda557
Fousq <fous363@gmail.com>
2019-08-30 15:50:44 +0600

    Impl API work with VK API

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import vk_api
 
+MAX_COMMENTS = 100
+
 """
     Outter api class for working with Vk api
     req: login and password of user to work with vk api
@@ -17,5 +19,12 @@ class API():
         @return return list of comments
         Goal: get avaliable comments of the post
     """
-    def get_comments(self):
-        pass
+    def get_comments(self, group_id: int, post_id: int):
+        response = self.api.wall.getComments(owner_id=group_id, post_id=post_id, count=MAX_COMMENTS)
+        print("items: " + str(response['items']))
+        print("items type: " + str(type(response['items'])))
+        print(response)
+        comments = list()
+        for comment in response['items']:
+            comments.append(comment['text'])
+        return comments
--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import engine
+import api
+
+def engine_test():
+    word_engine = engine.Engine()
+    words = word_engine.process("интересный, умный")
+    print(words)
+
+def api_test():
+    API = api.API("+77021165878", "1234fifa")
+    print(API)
+    comments = API.get_comments(-37763998, 2455537)
+    print(comments)
 
 if __name__ == "__main__":
-    engine = engine.Engine()
-    words = engine.process("интересный, умный")
-    print(words)
+    engine_test()
+    api_test()
